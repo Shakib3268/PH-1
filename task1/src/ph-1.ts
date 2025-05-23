2062eea9714144988ab2bfde84411347acf9fe4a
Shakib3268 <shakibalnur3268@gmail.com>
2025-05-24 01:01:24 +0600

    vul

--- task1/src/ph-1.ts
+++ task1/src/ph-1.ts
@@ -2,9 +2,4 @@
     function formatString(input: string, toUpper: boolean = true): string{
         return toUpper ? input.toUpperCase() : input.toLowerCase();
     }
-
-    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
-        return items.filter(item => item.rating >= 4);
-    }
-
 }
