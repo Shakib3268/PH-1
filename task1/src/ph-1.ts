1bbf02d99909d8970e54dd487f73c5eaa3f94eab
Shakib3268 <shakibalnur3268@gmail.com>
2025-05-24 00:59:53 +0600

    {commit_message}

--- task1/src/ph-1.ts
+++ task1/src/ph-1.ts
@@ -3,6 +3,8 @@
         return toUpper ? input.toUpperCase() : input.toLowerCase();
     }
 
-    
+    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+        return items.filter(item => item.rating >= 4);
+    }
 
 }
